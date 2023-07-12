27f33df665da9726a485fa97d28d3478192bcea0
Efen Stakes <efenstakes101@gmail.com>
2023-07-12 14:11:23 +0300

    add product card component

---
+++ src/components/product_card/component.tsx
@@ -0,0 +1,95 @@
+"use client"
+import React from 'react'
+import { Button, } from '@mui/material'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+import { useWindowWidth } from '@react-hook/window-size'
+
+
+// models
+import { IProduct } from '@/models/product'
+
+
+// styles
+import { containerVariants, itemVariants, slideUpVariants, } from '@/styles/variants'
+import styles from './component.module.css'
+
+
+
+type ProductCardProps = {
+    product: IProduct
+}
+const ProductCard = ({ product: { image, name, description, discount, href, }, }: ProductCardProps) => {
+    const width = useWindowWidth()
+    const isMobile = width < 1000
+
+
+    return (
+        <motion.div
+            className={styles.card}
+            variants={containerVariants}
+            initial="initial"
+            animate="animate"
+        >
+
+            {/* image */}
+            <Image
+                src={image as string}
+                alt='Discount Image'
+                width={100}
+                height={200}
+                className={styles.image}
+            />
+
+
+            <motion.div className={styles.cardContentOff}>
+                { discount }% Off
+            </motion.div>
+
+            {/* content */}
+            <motion.div className={styles.cardContent}>
+
+                <motion.p className={styles.cardContentTitle}>
+                    { name }
+                </motion.p>
+
+                <motion.p className={styles.cardContentDescription}>
+                    { description }
+                </motion.p>
+
+
+                {
+                    isMobile &&
+                        <motion.div variants={slideUpVariants}>
+                            <Button
+                                variant='contained'
+                                disableElevation
+                                href={href}
+                                size='large'
+                            >
+                                Buy Now
+                            </Button>
+                        </motion.div>
+                }
+
+                {
+                    !isMobile &&
+                        <div className={styles.cardContentCta}>
+                            <Button
+                                variant='contained'
+                                disableElevation
+                                href={href}
+                                size='large'
+                            >
+                                Buy Now
+                            </Button>
+                        </div>
+                }
+
+            </motion.div>
+
+        </motion.div>
+    )
+}
+
+export default ProductCard
