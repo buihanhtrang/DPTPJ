49bbf6538a60760996e91f65d0f89a2b43e98b02
Efen Stakes <efenstakes101@gmail.com>
2023-07-12 19:39:01 +0300

    add logo

--- src/components/more/component.tsx
+++ src/components/more/component.tsx
@@ -5,7 +5,7 @@ import moment from 'moment'
 
 
 // styles
-import { containerVariants, itemVariants } from '@/styles/variants'
+import { containerVariants, itemVariants, logoVariants, scaledLogoContainerVariants } from '@/styles/variants'
 import styles from './component.module.css'
 
 
@@ -18,6 +18,17 @@ const MoreCard = () => {
     return (
         <div className={styles.card}>
 
+            {/* image */}
+            <motion.div variants={itemVariants} className={styles.imageContainer}>
+                <Image
+                    src={'/assets/cozy-1.jpg'}
+                    alt='Discount Image'
+                    fill
+                    className={styles.image}
+                    loader={LoaderProp}
+                />
+            </motion.div>
+
             {/* content */}
             <motion.div
                 variants={containerVariants}
@@ -26,35 +37,51 @@ const MoreCard = () => {
                 className={styles.cardContent}
             >
 
-                <motion.p className={styles.cardContentTitle} variants={itemVariants}>
-                    More Cozy.
-                </motion.p>
-
-                <motion.p className={styles.cardContentSubTitle} variants={itemVariants}>
-                    Coming
-                </motion.p>
-
-                <motion.p className={styles.cardContentDescription} variants={itemVariants}>
-                    This 3D version of Ashley&apos;s Furniture is still in beta. We will be updating it gradually to reflect all products. Beta period will end in 2024.
-                </motion.p>
-
-                <motion.div className={styles.cardContentOff} variants={itemVariants}>
-                    { moment(new Date("2024-01-01")).fromNow() }
-                </motion.div>
-
-                </motion.div>
-
-                {/* image */}
-                <motion.div variants={itemVariants} className={styles.imageContainer}>
-                    <Image
-                        src={'/assets/cozy-1.jpg'}
-                        alt='Discount Image'
-                        fill
-                        className={styles.image}
-                        loader={LoaderProp}
+                <motion.svg
+                    width="40"
+                    height="43"
+                    viewBox="0 0 48 43"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    variants={scaledLogoContainerVariants}
+                    className={styles.logo}
+                >
+                    <motion.path
+                        d="M2 20L24 3L46 20V41H2V20Z"
+                        fill="transparent"
+                        stroke="white"
+                        strokeWidth="8"
+                        pathLength={1}
+                        strokeDasharray="1 1"
+                        variants={logoVariants}
+                        className={styles.logoPath}
                     />
-                </motion.div>
+                </motion.svg>
 
+
+                <motion.div
+                    variants={containerVariants}
+                    className={styles.cardContentColumn}
+                >
+                    <motion.p className={styles.cardContentTitle} variants={itemVariants}>
+                        More.
+                    </motion.p>
+
+                    <motion.p className={styles.cardContentSubTitle} variants={itemVariants}>
+                        Cozy Coming
+                    </motion.p>
+
+                    <motion.p className={styles.cardContentDescription} variants={itemVariants}>
+                        This 3D version of Ashley&apos;s Furniture is still in beta. We will be updating it gradually to reflect all products. Beta period will end in 2024.
+                    </motion.p>
+
+                    <motion.div className={styles.cardContentOff} variants={itemVariants}>
+                        { moment(new Date("2024-01-01")).fromNow() }
+                    </motion.div>
+
+                    </motion.div>
+
+                </motion.div>
             
         </div>
     )
