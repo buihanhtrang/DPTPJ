bea108c1eeac52a5b0551ec9d6f6a034a90c40be
Efen Stakes <efenstakes101@gmail.com>
2023-07-12 17:14:40 +0300

    add 3d configurator model

---
+++ src/models/configuration.ts
@@ -0,0 +1,11 @@
+
+export interface IConfiguratorOption {
+    colors: Array<string>
+    title: string
+    selectedColor: string
+}
+
+export interface IConfiguration {
+    options: IConfiguratorOption
+    title: string
+}
