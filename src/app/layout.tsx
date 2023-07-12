a463d8f6ca5944afd8eb187febb6944d7bcf0656
Efen Stakes <efenstakes101@gmail.com>
2023-07-12 22:17:12 +0300

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Raleway, Lato, Montserrat, Open_Sans } from 'next/font/google'
 import Providers from './providers'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Open_Sans({ subsets: ['latin'], })
 
 export const metadata: Metadata = {
   title: 'Ashleys Furniture',
