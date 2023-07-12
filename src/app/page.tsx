13ac3236551c307b444b3ef344abcc2a2b1501dc
Efen Stakes <efenstakes101@gmail.com>
2023-07-12 14:12:44 +0300

    add home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,81 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import Appbar from '@/components/appbar/component'
+import DiscountCard from '@/components/discounts_card/component'
+import { useRef } from 'react';
+import ProductCard from '@/components/product_card/component'
+import { motion } from 'framer-motion'
+import { IProduct } from '@/models/product'
+import { containerVariants, itemVariants } from '@/styles/variants'
 
+
+const products: Array<IProduct> = [
+  {
+    image: '/assets/cozy-1.jpg',
+    name: 'Round Chair',
+    description: 'Some Description here',
+    discount: 78,
+    href: '/'
+  },
+  {
+    image: '/assets/cozy-2.jpg',
+    name: 'Sofa Chair',
+    description: 'Some Description here',
+    discount: 56,
+    href: '/'
+  },
+]
 export default function Home() {
+  const shopRef = useRef<null | HTMLDivElement>(null)
+
+  const scrollToShop = () => {
+    shopRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={styles.page}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* app bar */}
+      <Appbar />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <motion.div
+        className={styles.pageContent}
+        variants={containerVariants}
+        initial="initial"
+        animate="animate"
+      >
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        {/* discount card */}
+        <DiscountCard scrollToShop={scrollToShop} />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div ref={shopRef} />
+
+
+        {/* list of product sections */}
+
+        {/* top deals section */}
+        <motion.h1 variants={itemVariants} className={styles.productSectionTitle}>
+          Top Deals
+        </motion.h1>
+        <motion.div className={styles.productGrid} variants={containerVariants}>
+          {
+            products.map((product, i)=> {
+
+              return (
+                <ProductCard key={i} product={product} />
+              )
+            })
+          }
+        </motion.div>
+
+
+      </motion.div>
+
+      <div style={{ height: '10vh', }} />
+
+    </div>
   )
 }
